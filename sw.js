c70d79aeb4077d5d0bd48a98bb3fe5f7d538376c
CheroMarket <wilmeragui.88@icloud.com>
2026-02-15 16:27:08 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,20 +2,17 @@ const CACHE_NAME = 'wilstaxtion-v1';
 const ASSETS = [
   './',
   './index.html',
+  './manifest.json',
   'https://unpkg.com/leaflet/dist/leaflet.css',
-  'https://unpkg.com/leaflet/dist/leaflet.js',
-  'https://unpkg.com/leaflet-routing-machine/dist/leaflet-routing-machine.css',
-  'https://unpkg.com/leaflet-routing-machine/dist/leaflet-routing-machine.js'
+  'https://unpkg.com/leaflet/dist/leaflet.js'
 ];
 
-// Instalación y guardado de archivos críticos
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
 });
 
-// Estrategia: Buscar en internet, si falla usar el caché
 self.addEventListener('fetch', (e) => {
   e.respondWith(
     fetch(e.request).catch(() => caches.match(e.request))
